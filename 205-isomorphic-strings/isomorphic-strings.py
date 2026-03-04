7165a9d9d458b0e6ee64e2bffb2a7718e52e2138
Vedant Kakade <151349713+Vedant22-prog@users.noreply.github.com>
2026-03-04 23:58:46 +0530

    Time: 15 ms (26.82%) | Memory: 13.4 MB (77.65%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        for i in range(len(s)):
+            if s.index(s[i]) != t.index(t[i]):
+                return False
+        return True
+        
