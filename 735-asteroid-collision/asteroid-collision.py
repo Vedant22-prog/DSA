822515da6a86fda74908c4eec8a6802a7efcc896
Vedant Kakade <151349713+Vedant22-prog@users.noreply.github.com>
2026-08-08 12:02:46 +0530

    Time: 11 ms (41.58%) | Memory: 13.2 MB (39.62%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+
+        stack = []
+
+        for asteroid in asteroids:
+
+            while stack and stack[-1] > 0 and asteroid < 0:
+
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue
+
+                elif stack[-1] == -asteroid:
+                    stack.pop()
+
+                break
+
+            else:
+                stack.append(asteroid)
+
+        return stack
