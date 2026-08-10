34de679d77732b25034ccfec5a497e8fc461c165
Vedant Kakade <151349713+Vedant22-prog@users.noreply.github.com>
2026-08-10 23:58:49 +0530

    Time: 289 ms (36.93%) | Memory: 29.1 MB (16.00%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,31 +1,24 @@
+from collections import deque
+
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
 
-        n = len(nums)
-
-        if k == 1:
-            return nums
-
-        left = [0] * n
-        right = [0] * n
-
-        # Left-to-right maximum
-        for i in range(n):
-            if i % k == 0:
-                left[i] = nums[i]
-            else:
-                left[i] = max(left[i - 1], nums[i])
-
-        # Right-to-left maximum
-        for i in range(n - 1, -1, -1):
-            if i == n - 1 or (i + 1) % k == 0:
-                right[i] = nums[i]
-            else:
-                right[i] = max(right[i + 1], nums[i])
-
+        dq = deque()
         result = []
 
-        for i in range(n - k + 1):
-            result.append(max(right[i], left[i + k - 1]))
+        for i in range(len(nums)):
+
+            # Remove expired elements
+            while dq and dq[0] <= i - k:
+                dq.popleft()
+
+            # Remove smaller elements
+            while dq and nums[dq[-1]] <= nums[i]:
+                dq.pop()
+
+            dq.append(i)
+
+            if i >= k - 1:
+                result.append(nums[dq[0]])
 
         return result
