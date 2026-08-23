e0f1c8d3f869b45c76109f503a630b00b04a1415
Vedant Kakade <151349713+Vedant22-prog@users.noreply.github.com>
2026-08-23 12:30:36 +0530

    Time: 3256 ms (5.07%) | Memory: 18 MB (34.71%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -0,0 +1,12 @@
+class KthLargest:
+
+    def __init__(self, k, nums):
+        self.k = k
+        self.nums = nums
+
+    def add(self, val):
+        self.nums.append(val)
+
+        self.nums.sort(reverse=True)
+
+        return self.nums[self.k - 1]
