df82c6004db1398d1e9718599a52c97691f3c7ee
Vedant Kakade <151349713+Vedant22-prog@users.noreply.github.com>
2026-08-23 12:45:02 +0530

    Time: 6 ms (84.81%) | Memory: 13.9 MB (97.13%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def topKFrequent(self, nums, k):
+
+        count = {}
+
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+
+        sorted_nums = sorted(
+            count,
+            key=count.get,
+            reverse=True
+        )
+
+        return sorted_nums[:k]
